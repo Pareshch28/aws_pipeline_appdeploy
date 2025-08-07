df8f3faa540ba48c6a8cb2e4b98b635bf9b84417
Paresh ch <144373102+Pareshch28@users.noreply.github.com>
2025-08-07 13:36:06 +0530

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+mkdir -p scripts
+echo -e "#!/bin/bash\nrm -rf /var/www/html/index.html" > scripts/cleanup.sh
+chmod +x scripts/cleanup.sh
+
+rm -f /var/www/html/index.html
 rm -f /var/www/html/index.html
 
